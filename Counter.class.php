e8a873311ea53116c1998587429a6110f7239cd2
Simplecov <simplecov@gmail.com>
2017-07-05 17:05:37 +0300

    Создание нужной таблицы, почти законченная запись в таблицу

--- Counter.class.php
+++ Counter.class.php
@@ -8,6 +8,8 @@ class CounterScore{
 
     private $request;
 
+    private $tableName;
+
     public function __construct()
     {
         $this->pluginFolder = plugin_dir_url(__FILE__);
@@ -16,7 +18,7 @@ class CounterScore{
         /**
          * @TODO переделать это на register_activation_hook
          */
-        $this->createDBTable();
+        $this->dbTableCreate();
         //register_activation_hook( __FILE__, array( '\Simplecov\CounterScore', 'createDBTable' ) );
     }
 
@@ -76,25 +78,50 @@ class CounterScore{
     /**
      * Создание таблицы
      */
-    public function createDBTable()
+    private function dbTableCreate()
     {
         global $wpdb;
-        $tableName = $wpdb->prefix . 'counter_score_plugin';
+        $this->tableName = $wpdb->prefix . 'counter_score_plugin';
 
-        if($wpdb->get_var("SHOW TABLES LIKE '$tableName'") != $tableName)
+        if($wpdb->get_var("SHOW TABLES LIKE '$this->tableName'") != $this->tableName)
         {
-            $sql = "CREATE TABLE " . $tableName . " (
+            $sql = "CREATE TABLE " . $this->tableName . " (
               id int NOT NULL AUTO_INCREMENT,
-              time int DEFAULT '0' NOT NULL,
-              name int NOT NULL,
-              text int NOT NULL,
-              url VARCHAR(55) NOT NULL,
+              firstname int NOT NULL,
+              lastname int NOT NULL,
+              appartment int NOT NULL,
+              month int NOT NULL,
+              year int NOT NULL,
+              water_cold_1 int NOT NULL,
+              water_cold_2 int NOT NULL,
+              water_hot_1 int NOT NULL,
+              water_hot_2 int NOT NULL,
+              electricity int NOT NULL,
               UNIQUE KEY id (id)
             );";
 
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
             dbDelta($sql);
         }
+    }
+
+    public function dbWrite($array)
+    {
+        global $wpdb;
+
+        if(is_array($array))
+        {
+            $data = array();
+            foreach ($array as $key => $value)
+            {
+                $data[$key] = trim($value);
+            }
+
+
+
+        }
+        else
+            return false;
     }
 
 }
